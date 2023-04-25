b73b2b5174a74146fd5424a9ff90d710aa9a3719
Andriw Tapanes <andriwtapanes@gmail.com>
2023-04-24 22:30:50 -0400

    layout changes

--- src/components/InputSin.tsx
+++ src/components/InputSin.tsx
@@ -27,24 +27,32 @@ const InputSin = () => {
 
   return (
     <div>
-      <h1 className="text-8xl mb-20 ml-24">{karma}</h1>
-      <h1>Enter Sin:</h1>
-      <input
-        placeholder="Enter text"
-        className="text-black p-2 rounded-xl"
-        onChange={handleChange}
-        value={sin}
-        onKeyDown={handleKeyDown}
-      />
-      <button className="bg-red-500 p-2 ml-2 rounded-xl" onClick={onAddClick}>
-        ADD
-      </button>
-      <div>
-        {sins.map((sin) => (
-          <div className="bg-stone-400 mt-2 mb-2 p-2 rounded-full " key={sin}>
-            {sin}
-          </div>
-        ))}
+      <h1 className="text-3xl flex justify-center pb-20">Hell Counter</h1>
+      <div className="flex flex-row static">
+        <div className="absolute left-80">
+          {sins.map((sin) => (
+            <div className="bg-stone-400 mt-2 mb-2 p-2 rounded-full " key={sin}>
+              {sin}
+            </div>
+          ))}
+        </div>
+        <div className="absolute bottom-80 top-60 right-60">
+          <h1 className="text-8xl mb-20 ml-24">{karma}</h1>
+          <h2>Enter Sin:</h2>
+          <input
+            placeholder="Enter text"
+            className="text-black p-2 rounded-xl"
+            onChange={handleChange}
+            value={sin}
+            onKeyDown={handleKeyDown}
+          />
+          <button
+            className="bg-red-500 p-2 ml-2 rounded-xl"
+            onClick={onAddClick}
+          >
+            ADD
+          </button>
+        </div>
       </div>
     </div>
   );
