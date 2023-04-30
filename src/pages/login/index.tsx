fd31eaf5ebf1684f6c9bcb4977334c228f22ecdd
Andriw Tapanes <andriwtapanes@gmail.com>
2023-04-29 23:10:44 -0400

    working on login and backend

---
+++ src/pages/login/index.tsx
@@ -0,0 +1,12 @@
+import Head from "next/head";
+import React from "react";
+
+export default function login() {
+  return (
+    <React.Fragment>
+      <Head>
+        <title>Login</title>
+      </Head>
+    </React.Fragment>
+  );
+}
